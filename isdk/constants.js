802b43515336ed7cd32d0e9c63c08be507ac35b0
D3685 <12345678>
2019-09-24 17:26:33 +0800

    增加avatar常量

--- isdk/constants.js
+++ isdk/constants.js
@@ -21,6 +21,19 @@ const CONSTANTS = {
   },
   IM_MSG_TYPE: {
     PLAIN_TEXT: 'PLAIN_TEXT'
+  },
+  avatars: {
+    avatar1: require('@assets/images/avatar/avatar1.jpg'),
+    avatar2: require('@assets/images/avatar/avatar2.jpg'),
+    avatar3: require('@assets/images/avatar/avatar3.jpg'),
+    avatar4: require('@assets/images/avatar/avatar4.jpg'),
+    avatar5: require('@assets/images/avatar/avatar5.jpg'),
+    avatar6: require('@assets/images/avatar/avatar6.jpg'),
+    avatar7: require('@assets/images/avatar/avatar7.jpg'),
+    avatar8: require('@assets/images/avatar/avatar8.jpg'),
+    avatar9: require('@assets/images/avatar/avatar9.jpg'),
+    avatar10: require('@assets/images/avatar/avatar10.jpg'),
+    avatar11: require('@assets/images/avatar/avatar11.jpg')
   }
 }
 export default CONSTANTS
