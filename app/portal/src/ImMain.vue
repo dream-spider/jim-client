55b023e6f37c6049ba3caf8e5775dab211b86da9
Hao Peng <phshy0607@outlook.com>
2019-10-16 00:25:12 +0800

    start some rxjs research

--- app/portal/src/ImMain.vue
+++ app/portal/src/ImMain.vue
@@ -19,6 +19,7 @@
 
 <script>
 import { net, user } from '@isdk'
+import { of, Observable, from } from 'rxjs'
 export default {
   name: 'ImMain',
   methods: {
@@ -36,6 +37,44 @@ export default {
   },
   created () {
     this.queryCustomeServiceList()
+
+
+    // const stream$ = of('1', '2')
+
+    // stream$.subscribe((v) => {
+    //   console.log(v)
+    // })
+    // stream$.subscribe((v) => {
+    //   console.log(v)
+    // })
+    // function timesTen (numbers$) {
+    //   let result$ = Observable.create((observer) => {
+    //     numbers$.subscribe({
+    //       next: (v) => observer.next(v * 10)
+    //     })
+    //   })
+
+    //   return result$
+    // }
+
+    // const arr$ = from([1, 2, 3])
+    // const ten$ = timesTen(arr$)
+
+    // ten$.subscribe(console.log)
+
+    // function formatDate (date$) {
+    //   let formatted$ = Observable.create((observer) => {
+    //     date$.subscribe((v) => {
+    //       observer.next(`${v.getUTCFullYear()}-${v.getUTCMonth() + 1}-${v.getUTCDate() + 1}`)
+    //     })
+    //   })
+    //   return formatted$
+    // }
+
+    // const date$ = from([new Date(), new Date('2019-10-10'), new Date('2019-10-11')])
+    // const formatted$ = formatDate(date$)
+    // formatted$.subscribe(console.log)
+
   },
   data () {
     return {
